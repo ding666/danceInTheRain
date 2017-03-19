fdf380aac2952fcb60b6fd6dc7e394f2cdecd3b1
Chunping <chunping@ChunpingMacMini.local>
2017-03-18 23:09:44 -0400

    added action1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 var app = angular.module('dance', []);
 app.controller('actionCtrl', function($scope, $http) {
+    $scope.hideProjList = false;
     console.log("Inside controller");
    $http.get("listProjects.php")
    .then(function (response) {
        console.log(response);
        $scope.projects = response.data;
     });
+
+    $scope.projDetail = function(id) {
+        console.log(id);
+    }
+ 
+    $scope.createProj = function() {
+        console.log("Hello");
+        $scope.showCreateProj = true;
+        $scope.hideProjList = true;
+    }
 });
  
